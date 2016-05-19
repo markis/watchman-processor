bb37cfcdb765eacee6ddd8451e0a45612b99b7fc
Markis Taylor <m@rkis.cc>
2016-05-19 10:02:00 -0700

    fix stdout max error, enhanced logging

--- src/sync.js
+++ src/sync.js
@@ -6,13 +6,11 @@ function Sync(config, terminal) {
   this.config = config;
   this.terminal = terminal;
   this.rsyncCmd = this.config.rsyncCmd || 'rsync';
-  this.maxFileLength = this.config.maxFileLength || 10000;
+  this.maxFileLength = this.config.maxFileLength || 100;
 }
 
 Sync.prototype.syncFiles = function syncFiles(subConfig, src, dest, files) {
-  const terminal = this.terminal;
   files = files || [];
-  terminal.debug(JSON.stringify(files, null, 2));
   files = files.map(function(file) { return file.name });
   files = files.filter(function(file) {
     return file.indexOf('.sass-cache/') === -1 &&
@@ -35,7 +33,7 @@ Sync.prototype._syncAllFiles = function _syncAllFiles(subConfig, src, dest) {
   const excludes = " --exclude '.idea' --exclude '.git' --exclude '.sass-cache'";
 
   return new Promise(function(resolve, reject) {
-    const cmd = [rsyncCmd, '-avz --delete', src, dest, excludes].join(' ');
+    const cmd = [rsyncCmd, '-avz --stats --delete', src, dest, excludes].join(' ');
     terminal.debug(cmd);
     exec(cmd, null, getExecCallback(resolve, reject));
   });
@@ -51,7 +49,7 @@ Sync.prototype._syncSpecificFiles = function _syncSpecificFiles(subConfig, src, 
   const includes = " --include '" + files.join("' --include '") + "'";
 
   return new Promise(function(resolve, reject) {
-    const cmd = [rsyncCmd, '-avz --delete', includes, excludes, src, dest].join(' ');
+    const cmd = [rsyncCmd, '-avz --stats --delete', includes, excludes, src, dest].join(' ');
     terminal.debug(cmd);
     exec(cmd, null, getExecCallback(resolve, reject));
   });
