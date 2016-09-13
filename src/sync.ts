b2e3b88285938c9e64e469518585ca083d484f78
Markis Taylor <m@rkis.cc>
2016-09-13 12:13:27 -0700

    adding polyfills for node 0.12

--- src/sync.ts
+++ src/sync.ts
@@ -32,9 +32,8 @@ export default class SyncImpl implements Sync {
   public syncFiles(subConfig: SubConfig, fbFiles?: SubscriptionResponseFile[]): Promise<void> {
     const
       ignoreFolders = subConfig.ignoreFolders,
-      files: string[] = (fbFiles || [])
-        .map(file => file.name)
-        .filter(file => ignoreFolders.findIndex(folder => file.startsWith(folder)) === -1);
+      filesNames: string[] = (fbFiles || []).map(file => file.name),
+      files = filesNames.filter(file => ignoreFolders.findIndex(folder => file.startsWith(folder)) === -1);
 
     // if there are too many files, it might just be better to let rsync figure out what
     // needs to be synced
@@ -105,4 +104,42 @@ function unique(arr: string[]): string[] {
   return arr.filter((item: string) => {
     return seen.has(item) ? false : !!seen.set(item, true);
   });
+}
+
+
+/*
+ *
+ *  Polyfills for basic javascript functionality that doesn't exist in 0.12
+ * 
+ */ 
+if (!Array.prototype.findIndex) {
+  Array.prototype.findIndex = function(predicate) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.findIndex called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    const list = Object(this),
+      length = list.length,
+      thisArg = arguments[1];
+    
+    let value: any;
+
+    for (let i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
 }
